90a50c6e494784d5a198897d84aa651ec3777777
Santosh <santosh.ramavath@gmail.com>
2016-01-13 21:32:04 -0600

    second commit

--- sum_test.py
+++ sum_test.py
@@ -8,6 +8,8 @@ class sum_test(unittest.TestCase):
 	def test_sum_fail(self):
 		self.assertFalse(sum.sum(4,5) == 8)
 
+	def test_fail(self):
+		self.assertTrue(sum.sum(8,9), 17)
 
 if __name__ == '__main__':
     unittest.main()
